fcfae9ed117b3a67808a7b5f5303deab2088e5bf
AilsaYang <ailsayang831031@gmail.com>
2023-10-18 16:37:07 +0800

    chore: remove test1 test2 test3

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ import Offer from "components/Offer/Offer";
 import Reservation from "components/Reservation/Reservation";
 import Testimonial from "components/Testimonial/Testimonial";
 import Footer from "components/Footer/Footer";
-import Test1 from "components/Test1/Test1";
-import Test2 from "components/Test2/Test2";
-import Test3 from "components/Test3/Test3";
 
 function App() {
   return (
@@ -30,9 +27,6 @@ function App() {
       <Reservation />
       <Testimonial />
       <Footer />
-      <Test2 />
-      <Test1 />
-      <Test3 />
     </div>
   );
 }
