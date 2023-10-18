5b79a69c1f2cbb3c88b2939921d8950281bf8fa2
AilsaYang <ailsayang831031@gmail.com>
2023-10-18 16:39:01 +0800

    feat: add Data

---
+++ src/Data.jsx
@@ -0,0 +1,282 @@
+// For Features jsx
+import freeDelivery from "assets/images/free-delivery.png";
+import certified from "assets/images/certified.png";
+import highQuality from "assets/images/high-quality.png";
+import roastedCoffee from "assets/images/roasted-coffee.png";
+
+// For Menu
+import coffeeProduct from "assets/images/coffeeProduct.png";
+
+// For Gallery
+import galleryPic1 from "assets/images/galleryPic1.jpeg";
+import galleryPic2 from "assets/images/galleryPic2.jpeg";
+import galleryPic3 from "assets/images/galleryPic3.jpeg";
+import galleryPic4 from "assets/images/galleryPic4.jpeg";
+import galleryPic5 from "assets/images/galleryPic5.jpeg";
+import galleryPic6 from "assets/images/galleryPic6.jpeg";
+
+// For Offer
+import offer1 from "assets/images/offer1.jpeg";
+import offer2 from "assets/images/offer2.jpeg";
+import offer3 from "assets/images/offer3.jpeg";
+import offer4 from "assets/images/offer4.jpeg";
+import offer5 from "assets/images/offer5.jpeg";
+
+// For Testimonial
+import person1 from "assets/images/customers/person1.png";
+import person2 from "assets/images/customers/person2.png";
+import person3 from "assets/images/customers/person3.png";
+import person4 from "assets/images/customers/person4.png";
+import person5 from "assets/images/customers/person5.png";
+import person6 from "assets/images/customers/person6.jpeg";
+import person7 from "assets/images/customers/person7.png";
+import person8 from "assets/images/customers/person8.png";
+
+//For Each section
+export const links = [
+  { name: "Home", path: "home" },
+  { name: "About", path: "about" },
+  { name: "Features", path: "features" },
+  { name: "Menu", path: "menu" },
+  { name: "Gallery", path: "gallery" },
+  { name: "Offer", path: "offer" },
+  { name: "Reservation", path: "reservation" },
+];
+
+// For Features jsx
+export const features = [
+  {
+    id: 1,
+    img: freeDelivery,
+    title: "Free Shipping",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 2,
+    img: certified,
+    title: "Organic Certified",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 3,
+    img: highQuality,
+    title: "High Quality",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 4,
+    img: roastedCoffee,
+    title: "Proper Roasting",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+];
+
+// For Menu
+export const menu = [
+  {
+    id: 1,
+    img: coffeeProduct,
+    title: "Americano Coffee",
+    description: "Milk with vanilla flavored",
+    price: 50.25,
+  },
+  {
+    id: 2,
+    img: coffeeProduct,
+    title: "Cappuccino Arabica",
+    description: "Milk with vanilla flavored",
+    price: 70.25,
+  },
+  {
+    id: 3,
+    img: coffeeProduct,
+    title: "Milk Cream Coffee",
+    description: "Milk with vanilla flavored",
+    price: 30.25,
+  },
+  {
+    id: 4,
+    img: coffeeProduct,
+    title: "Special Raw Coffee",
+    description: "Milk with vanilla flavored",
+    price: 80.25,
+  },
+  {
+    id: 5,
+    img: coffeeProduct,
+    title: "Fresh Black Coffee",
+    description: "Milk with vanilla flavored",
+    price: 90.25,
+  },
+  {
+    id: 6,
+    img: coffeeProduct,
+    title: "Cappuccino Arabica",
+    description: "Milk with vanilla flavored",
+    price: 70.25,
+  },
+  {
+    id: 7,
+    img: coffeeProduct,
+    title: "Milk Cream Coffee",
+    description: "Milk with vanilla flavored",
+    price: 120.25,
+  },
+  {
+    id: 8,
+    img: coffeeProduct,
+    title: "Cold Coffee",
+    description: "Milk with vanilla flavored",
+    price: 40.25,
+  },
+];
+
+// For Gallery
+export const gallery = [
+  {
+    id: 1,
+    img: galleryPic1,
+    title: "By the Window",
+  },
+  {
+    id: 2,
+    img: galleryPic2,
+    title: "Coffee and Desserts",
+  },
+  {
+    id: 3,
+    img: galleryPic3,
+    title: "Front Door",
+  },
+  {
+    id: 4,
+    img: galleryPic4,
+    title: "Corner",
+  },
+  {
+    id: 5,
+    img: galleryPic5,
+    title: "Milk and Coffee",
+  },
+  {
+    id: 6,
+    img: galleryPic6,
+    title: "Cappuccino",
+  },
+];
+
+// For Offer
+export const offer = [
+  {
+    id: 1,
+    img: offer1,
+    title: "Cappuccino Arabica",
+    discount: "Get 40% Offer",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 2,
+    img: offer2,
+    title: "Cappuccino Arabica",
+    discount: "Get 60% Offer",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 3,
+    img: offer3,
+    title: "Cappuccino Arabica",
+    discount: "Get 50% Offer",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 4,
+    img: offer4,
+    title: "Cappuccino Arabica",
+    discount: "Get 30% Offer",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+  {
+    id: 5,
+    img: offer5,
+    title: "Cappuccino Arabica",
+    discount: "Get 20% Offer",
+    description:
+      "It is a long established fact that a reader will be by the readable content of a page looking at its layout",
+  },
+];
+
+// For Testimonial
+export const testimonials = [
+  {
+    id: 1,
+    img: person1,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 2,
+    img: person2,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 3,
+    img: person3,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 4,
+    img: person4,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 5,
+    img: person5,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 6,
+    img: person6,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 7,
+    img: person7,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+  {
+    id: 8,
+    img: person8,
+    title: "Jane E Butt",
+    service: "Customer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium aliquam beatae similique numquam reiciendis magnam quia aut dignissimos incidunt molestiae.",
+  },
+];
