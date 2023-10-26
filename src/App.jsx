32a70bb439161403c825503aa9626b07d0f0dde5
AilsaYang <ailsayang831031@gmail.com>
2023-10-26 11:13:18 +0800

    feat: delete shopping cart router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import { HomePage, LoginPage, ProductPage, CartPage } from "pages";
 import ProductBeanDetail from "components/ProductBeanDetail/ProductBeanDetail";
-import ShoppingCart from "components/ShoppingCart/ShoppingCart";
 
 import "./App.scss"; //要在最後才import不然會無法RWD
 
@@ -21,7 +20,6 @@ function App() {
             element={<ProductBeanDetail />}
           />
           <Route path="cart" element={<CartPage />} />
-          <Route path="shopping-cart" element={<ShoppingCart />} />
         </Routes>
       </BrowserRouter>
     </div>
