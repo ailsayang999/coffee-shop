197b861f9c327572cb6934a254c869970349c1fb
AilsaYang <ailsayang831031@gmail.com>
2023-10-26 12:38:33 +0800

    feat: add cart item selection

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import "./cart.scss";
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
-import { BiSolidRightArrow, BiSolidLeftArrow } from "react-icons/bi";
+import {
+  BiSolidRightArrow,
+  BiSolidLeftArrow,
+  BiSolidChevronsDown,
+} from "react-icons/bi";
 import { products } from "Data";
 
 const CartItem = () => {
@@ -17,16 +21,23 @@ const CartItem = () => {
         />
         <div className="cart-product-details">
           <span className="cart-product-name">
-            <b>Product: </b> 巴西小丸子
+            <b>產品名稱: </b> 巴西小丸子
           </span>
 
           <span className="cart-product-id">
-            <b>ID: </b> 10051
+            <b>價格: </b> 35 元
           </span>
 
-          <span className="cart-product-selected">
-            <b>項目: </b> 濾掛
-          </span>
+          <div className="cart-selection-container">
+            <select className="cart-product-selection-box">
+              <option value={"濾掛式"}>濾掛式</option>
+              <option value={"半磅"}>半磅</option>
+            </select>
+
+            <div className="down-arrow-icon-container">
+              <BiSolidChevronsDown className="down-arrow-icon" />
+            </div>
+          </div>
         </div>
       </div>
       <div className="cart-product-price-detail">
@@ -48,6 +59,7 @@ const Cart = () => {
       <div className="cart-page">
         <div className="cart-container">
           <div className="cart-content-wrapper">
+            
             <h1 className="cart-title">Shopping Cart</h1>
             <div className="cart-top"></div>
 
@@ -133,6 +145,7 @@ const Cart = () => {
                 結帳去 <BiSolidRightArrow className="cart-btn-right-arrow" />
               </button>
             </div>
+
           </div>
         </div>
       </div>
