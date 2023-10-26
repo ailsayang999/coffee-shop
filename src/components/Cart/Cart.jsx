a8afe44928d7b8ee75133896f7f7f20efc911317
AilsaYang <ailsayang831031@gmail.com>
2023-10-26 11:25:37 +0800

    feat: create CartItem component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,43 @@ import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 
+
+
+const CartItem = () => {
+  return (
+    <div className="cart-product">
+      <div className="cart-product-detail">
+        <img
+          src="https://i0.wp.com/018-studio.com/wp-content/uploads/2022/06/%E5%B7%B4%E8%A5%BF%E5%B0%8F%E4%B8%B8%E5%AD%90.png?fit=540%2C540&ssl=1"
+          alt=""
+          className="cart-product-img"
+        />
+        <div className="cart-product-details">
+          <span className="cart-product-name">
+            <b>Product: </b> 巴西小丸子
+          </span>
+
+          <span className="cart-product-id">
+            <b>ID: </b> 10051
+          </span>
+
+          <span className="cart-product-selected">
+            <b>項目: </b> 濾掛
+          </span>
+        </div>
+      </div>
+      <div className="cart-product-price-detail">
+        <div className="cart-product-amount-container">
+          <AiFillMinusCircle className="amount-icon" />
+          <div className="cart-product-amount">2</div>
+          <AiFillPlusCircle className="amount-icon" />
+        </div>
+        <div className="cart-product-price">$200</div>
+      </div>
+    </div>
+  );
+};
+
 const Cart = () => {
   return (
     <>
@@ -19,7 +56,7 @@ const Cart = () => {
             <div className="cart-bottom">
               <div className="cart-info">
                 {/* 一個項目 */}
-                <div className="cart-product">
+                {/* <div className="cart-product">
                   <div className="cart-product-detail">
                     <img
                       src="https://i0.wp.com/018-studio.com/wp-content/uploads/2022/06/%E5%B7%B4%E8%A5%BF%E5%B0%8F%E4%B8%B8%E5%AD%90.png?fit=540%2C540&ssl=1"
@@ -48,40 +85,11 @@ const Cart = () => {
                     </div>
                     <div className="cart-product-price">$200</div>
                   </div>
-                </div>
+                </div> */}
 
-                <hr className="cart-product-hr" />
-                {/* 一個項目 */}
-                <div className="cart-product">
-                  <div className="cart-product-detail">
-                    <img
-                      src="https://i0.wp.com/018-studio.com/wp-content/uploads/2022/06/%E7%89%B9%E7%B4%9A%E8%97%8D%E5%B1%B1.png?fit=540%2C540&ssl=1"
-                      alt=""
-                      className="cart-product-img"
-                    />
-                    <div className="cart-product-details">
-                      <span className="cart-product-name">
-                        <b>Product: </b> 特級藍山 小象
-                      </span>
+                <CartItem />
 
-                      <span className="cart-product-id">
-                        <b>ID: </b> 10101
-                      </span>
-
-                      <span className="cart-product-selected">
-                        <b>項目: </b> 濾掛
-                      </span>
-                    </div>
-                  </div>
-                  <div className="cart-product-price-detail">
-                    <div className="cart-product-amount-container">
-                      <AiFillMinusCircle className="amount-icon" />
-                      <div className="cart-product-amount">3</div>
-                      <AiFillPlusCircle className="amount-icon" />
-                    </div>
-                    <div className="cart-product-price">$400</div>
-                  </div>
-                </div>
+                {/* <hr className="cart-product-hr" /> */}
               </div>
               <div className="cart-summary">
                 <h1 className="cart-summary-title">Oder Summary</h1>
