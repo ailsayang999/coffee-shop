fea8b645ace63467434792b742ec72e90618764b
AilsaYang <ailsayang831031@gmail.com>
2023-10-26 11:10:48 +0800

    feat: add cart page in index.js in page folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
 import ProductPage from "./ProductPage";
+import CartPage from "./CartPage";
 
-export { HomePage, ProductPage, LoginPage };
+export { HomePage, ProductPage, LoginPage, CartPage };
